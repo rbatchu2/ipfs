9531f54427863b4008046a5b503a1faa52776d0b
Ronit Batchu <ronitbatchu@Ronits-MacBook-Pro.local>
2025-03-16 23:56:21 -0700

    first function complete

---
+++ ipfs.py
@@ -0,0 +1,25 @@
+import requests
+
+API_KEY = '3b9177c4128330b81a72'
+API_SECRET = 'f72004d86684bd629c8ec3c3aa30c6bfda807262c40f4ec74870bef6c21593b6'
+
+def pin_to_ipfs(data):
+	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
+	
+    url = "https://api.pinata.cloud/pinning/pinFileToIPFS"
+    headers = {
+        "pinata_api_key": API_KEY,
+        "pinata_secret_api_key": API_SECRET
+    }
+    response = requests.post(url, headers=headers, json=data)
+    response_json = response.json()
+    cid = response_json.get("IpfsHash")
+	return cid
+
+def get_from_ipfs(cid,content_type="json"):
+	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
+	#YOUR CODE HERE	
+
+	assert isinstance(data,dict), f"get_from_ipfs should return a dict"
+	return data
+
